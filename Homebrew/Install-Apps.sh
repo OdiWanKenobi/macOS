93aa9fbd54c1a46292977f91bca70d63f73a4826
Alex Labrosse <alexander.labrosse@gmail.com>
2021-06-16 12:02:35 -0400

    Create Install-Apps.sh
    Installs the following applications using Homebrew:
    
    - Battle.net
    - Dashlane
    - Discord
    - Microsoft Edge
    - Microsoft Office
    - Microsoft Edge
    - Microsoft Teams
    - Microsoft Remote Desktop
    - Slack
    - Signal
    - WhatsApp
    - Zoom
    - GitHub Desktop
    - Visual Studio Code
    - NordVPN
    - PowerShell
    - Skype
    - TeamViewer
    - Tor Browser

---
+++ Homebrew/Install-Apps.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Battle.net
+brew install --cask battle-net
+
+# Dashlane
+brew install --cask dashlane
+
+# Discord
+brew install --cask discord
+
+# Microsoft Edge
+brew install --cask microsoft-edge
+
+# Microsoft Office
+brew install --cask microsoft-office
+
+# Microsoft Teams
+brew install --cask microsoft-teams
+
+# Microsoft Remote Desktop
+brew install --cask microsoft-remote-desktop
+
+# Slack
+brew install --cask slack
+
+# Signal
+brew install --cask signal
+
+# WhatsApp
+brew install --cask whatsapp
+
+# Zoom
+brew install --cask zoom
+
+# GitHub Desktop
+brew install --cask github
+
+# Visual Studio Code
+brew install --cask visual-studio-code
+
+# NordVPN
+brew install --cask nordvpn
+
+# PowerShell
+brew install --cask powershell
+
+# Skype
+brew install --cask skype
+
+# TeamViewer
+brew install --cask teamviewer
+
+# Tor Browser
+brew install --cask tor-browser
